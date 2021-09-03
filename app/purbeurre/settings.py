35c12214cf6e16460866c9d2492f6defb89f4c8e
unknown <fdlcm974@gmail.com>
2021-09-03 16:05:14 +0200

    test

--- app/purbeurre/settings.py
+++ app/purbeurre/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "NAME": os.environ.get("SQL_DATABASE", "pur-beurre"),
         "USER": os.environ.get("SQL_USER", "purbeurredev"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "purbeurretest"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "HOST": os.environ.get("SQL_HOST", "db"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
